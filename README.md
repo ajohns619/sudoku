aa39a7e01c8ebbef46f6753bd88a12e8f89b4d04
Alex Johnson <alex.johnson969@gmail.com>
2017-04-18 17:11:01 -0400

    Added Readme and length check

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Sudoku
+# Sudoku Checker
 
-Work-in-progress to learn python by creating program to check completed sudokus
+This program reads in a solved sudoku from a file and checks if it has been solved correctly.
+
+A correct sudoku has the following properties (and the program follows this logical reasoning):
+* The sudoku has 81 elements.
+* The smallest value in the sudoku is 1, and the largest value is 9.
+* No row contains any duplicate integers.
+* No column contains any duplicate integers.
+* No 9x9 square contains any duplicate integers.
+
+I created this program to learn python, especially I/O functionality and list comprehension.
--- sudoku-checker.py
+++ sudoku-checker.py
@@ -2,45 +2,45 @@ from sys import argv
 
 script, sudoku_name = argv
 
-# Gets 9x9 sudoku with specified filename
+# Gets 9x9 sudoku with specified filename and stores it in list
 def get_sudoku(name):
     sudoku = [];
-    f = open(name, "r")
+    file = open(name, "r")
+
     while True:
-        c = f.read(1)
+        # Read a single character
+        c = file.read(1)
         if not c:
             break
         elif c.isdigit():
             sudoku.append(int(c))
-
-    f.close()
+    file.close()
     return sudoku
 
 # Checks that the digits are between and include 1 and 9
-def check_min_max(s):
-    if max(s) != 9 or min(s) != 1:
+def check_min_max(sudoku):
+    if max(sudoku) != 9 or min(sudoku) != 1:
         return False
     return True
 
 # Checks that the rows have no duplicates
-def check_rows(s):
+def check_rows(sudoku):
     for row in range(9):
         row_elems = [0 for i in range(10)]
         for col in range(9):
-            elem = s[row * 9 + col]
+            elem = sudoku[row * 9 + col]
             if row_elems[elem] == 0:
                 row_elems[elem] = 1
             else:
                 return False
-
     return True
            
 # STUB
-def check_cols(s):
+def check_cols(sudoku):
     for col in range(9):
         col_elems = [0 for i in range(10)]
         for row in range(9):
-            elem = s[col * 9 + row]
+            elem = sudoku[col * 9 + row]
             if col_elems[elem] == 0:
                 col_elems[elem] = 1
             else:
@@ -49,21 +49,21 @@ def check_cols(s):
     return True
 
 # Checks that all squares have only one instance of any digit
-def check_squares(s):
+def check_squares(sudoku):
     for first_row in range(3):
         for first_col in range(3):
-            if not check_single_square(s, 3 * first_row, 3 * first_col):
+            if not check_single_square(sudoku, 3 * first_row, 3 * first_col):
                 return False
     return True
 
 
 # Checks that one specific square has only one instance of any digit
 # first_row and first_col are the coordinares of the upper left element of the square
-def check_single_square(s, first_row, first_col):
+def check_single_square(sudoku, first_row, first_col):
     square_elems = [0 for i in range(10)]
     for row in range(3):
         for col in range(3):
-            elem = s[(first_row + row) * 9 + (first_col + col)]
+            elem = sudoku[(first_row + row) * 9 + (first_col + col)]
             if square_elems[elem] == 0:
                 square_elems[elem] = 1
             else:
@@ -72,18 +72,20 @@ def check_single_square(s, first_row, first_col):
 
 # Calls all checks to validate sudoku
 # And operators require all checks to be true
-def validate_sudoku(s):
+def validate_sudoku(sudoku):
+    if (len(sudoku) != 81):
+        return False
     result  = True
-    result &= check_min_max(s)
-    result &= check_rows(s)
-    result &= check_cols(s)
-    result &= check_squares(s)
+    result &= check_min_max(sudoku)
+    result &= check_rows(sudoku)
+    result &= check_cols(sudoku)
+    result &= check_squares(sudoku)
 
     return result
 
 def main():
-    s = get_sudoku(sudoku_name)
-    result = validate_sudoku(s)
+    sudoku = get_sudoku(sudoku_name)
+    result = validate_sudoku(sudoku)
     print result  
 
 if __name__ == "__main__":
